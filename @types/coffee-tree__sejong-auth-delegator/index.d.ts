756ff13b1af775a3932c1804272a83eb105ba131
Im-Siyoun <rlagusdn3321@naver.com>
2024-06-04 11:02:13 +0900

    :heavy_plus_sign: 모듈 타입 선언 작성

--- @types/coffee-tree__sejong-auth-delegator/index.d.ts
+++ @types/coffee-tree__sejong-auth-delegator/index.d.ts
@@ -1,14 +1,26 @@
 declare module '@coffee-tree/sejong-auth-delegator' {
   export interface LoginRequestDto {
-      userId: string;
-      password: string;
+    userId: string;
+    password: string;
+  }
+
+  export interface ProfileResponseDto {
+    major: string;
+    studentCode: string;
+    name: string;
+    gradelevel: number;
+    userStatus: string;
+    completedSemesters: number;
+    verifiedSemesters: number;
   }
 
   export interface AuthDelegator {
-      getAuthenticatedJsessionId(loginRequestDto: LoginRequestDto): Promise<string>;
-      isAuthenticatedUser(loginRequestDto: LoginRequestDto): Promise<boolean>;
-      getUserProfile(loginRequestDto: LoginRequestDto): Promise<any>; // 이 부분은 실제 반환되는 사용자 프로필 객체의 형태에 따라 변경해야 합니다.
-      createLoginRequestDto(userId: string, password: string): LoginRequestDto;
+    getAuthenticatedJsessionId(
+      loginRequestDto: LoginRequestDto,
+    ): Promise<string>;
+    isAuthenticatedUser(loginRequestDto: LoginRequestDto): Promise<boolean>;
+    getUserProfile(loginRequestDto: LoginRequestDto): Promise<any>; // 이 부분은 실제 반환되는 사용자 프로필 객체의 형태에 따라 변경해야 합니다.
+    createLoginRequestDto(userId: string, password: string): LoginRequestDto;
   }
 
   const sejongAuthDelegator: () => AuthDelegator;
